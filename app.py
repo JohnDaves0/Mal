486fba762838a1f7bd3d418b6dc3ae364ca95f13
JohnDaves0 <davesjohn644@gmail.com>
2025-04-20 09:32:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,6 @@ def tts():
 
 import os
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
     app.run(host="0.0.0.0", port=port)
